5f0e93864a28ff415f2b2bbd5c2fc2c04425f800
Yaraslauna_Simaka <yaraslauna.simaka@gmail.com>
2017-06-02 17:29:08 +0300

    TICKET-1: [+] second middleware

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ let fs = require('fs');
 
 let app = new koa() ;
 
-app.use(function* () {
+app.use(function* (next) {
     this.body = 'Hello world!';
+    yield next;
 });
+
+app.use(function* (next) {
+    this.body += 'Hello world1!';
+
+});
+
 
 app.listen(3000, function() {
     console.log('Server running on https://localhost:3000')
