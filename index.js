2f5fe19523b1f96c0edbce23017566704f4096bf
Yaraslauna_Simaka <yaraslauna.simaka@gmail.com>
2017-06-02 16:30:46 +0300

    TICKET-1: [+] koa-request [+] hello world

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 let koa = require('koa');
 let fs = require('fs');
+
+let app = koa() ;
+
+app.use(function* () {
+    this.body = 'Hello world!';
+});
+
+app.listen(3000, function() {
+    console.log('Server running on https://localhost:3000')
+});
