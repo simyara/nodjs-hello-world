8a1939832804492ebfc5bacd9fcd386e32d771cd
Yaraslauna_Simaka <yaraslauna.simaka@gmail.com>
2017-06-06 09:37:43 +0300

    TICKET-1: [-] excess middlewares removed

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
+"use strict";
 let Koa = require('koa');
 let fs = require('fs');
 
 let router = require('./api');
 
-let app = new Koa() ;
+let app = new Koa();
 
 app.use(router.routes());
 
-app.use(function* (next) {
-    this.body += '1st: Hello world!     ';
-    yield next;
-    this.body += '3rd: Hello world!     ';
-});
-
-app.use(function* () {
-    this.body += '2nd: Hello world1';
-
-});
-
-app.listen(3000, function() {
-    console.log('Server running on https://localhost:3000')
+app.listen(3000, function () {
+    console.log('Server running on https://localhost:3000');
 });
