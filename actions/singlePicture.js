bf0109692ca52e4967d7f7ba73071cb780acffd1
Yaraslauna_Simaka <yaraslauna.simaka@gmail.com>
2017-06-08 12:58:29 +0300

    TICKET-4: [+] body parser middleware [+] object validation method [+] update single picture method

--- actions/singlePicture.js
+++ actions/singlePicture.js
@@ -1,9 +1,21 @@
 "use strict";
 
 let pictureServer = require('../services/pictureServer');
+let pictureValidator = require('../middleware/validator');
+
+function updateItem(origObj, dataToUpdate){
+    Object.keys(dataToUpdate).forEach(function(key){
+        if ((typeof dataToUpdate[key]) === 'object'){
+            updateItem(origObj[key], dataToUpdate[key]);
+        }
+        else {
+            origObj[key]=dataToUpdate[key];
+        }
+    });
+}
 
 module.exports = {
-    getOnePicture:  function* () {
+    getOnePicture: function*() {
         let pictureData = yield pictureServer.findOne(this.params.id);
 
         if (!pictureData) {
@@ -21,7 +33,28 @@ module.exports = {
             status: 'success',
             data: pictureData
         };
+    },
+    updateOnePicture: function*(next) {
+        let pictureData = yield pictureServer.findOne(this.params.id);
+
+        if (!pictureData) {
+            this.status = 404;
+            this.body = {
+                status: 'fail',
+                data: {
+                    error: 'Picture ot found'
+                }
+            };
+            return;
+        }
+
+        updateItem(pictureData, this.request.body);
+
+        this.body = {
+            status:'success',
+            data: pictureData
+        };
+
+        yield next;
     }
 };
-
-
--- api.js
+++ api.js
@@ -3,8 +3,16 @@
 let router = require('koa-router')();
 let aPictures = require('./actions/allPictures');
 let sPicture = require('./actions/singlePicture');
+let bodyParser = require('./middleware/bodyParser');
+let validator = require('./middleware/validator');
+
+
 
 router.get('/pictures', aPictures.getAllPictures);
+
 router.get('/pictures/:id', sPicture.getOnePicture);
+
+router.post('/pictures/:id', bodyParser.parseBody, validator.validateObject, sPicture.updateOnePicture);
+
 
 module.exports = router;
